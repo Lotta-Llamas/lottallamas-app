7a24b2113457d5bed55732f27e49c7649e7b1a91
Tom Schnaars <tschnaars@shift4.com>
2022-12-18 16:36:10 -0500

    init of ember app with latest version

---
+++ tests/integration/components/ui/nav/nav-login-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'llamas-app/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | ui/nav/nav-login', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    await render(hbs`<Ui::Nav::NavLogin />`);
+
+    assert.dom(this.element).hasText('');
+
+    // Template block usage:
+    await render(hbs`
+      <Ui::Nav::NavLogin>
+        template block text
+      </Ui::Nav::NavLogin>
+    `);
+
+    assert.dom(this.element).hasText('template block text');
+  });
+});
