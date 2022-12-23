77c77403dc0127e59051eddd642ecff654efbebe
Tom Schnaars <tschnaars@shift4.com>
2022-12-22 22:45:02 -0500

    reinstate walletId on comment model

--- app/models/comment.js
+++ app/models/comment.js
@@ -3,5 +3,6 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 export default class CommentModel extends Model {
 	@attr comment;
 	@attr postId;
+	@attr walletId;
 	@belongsTo('post') post;
 }
