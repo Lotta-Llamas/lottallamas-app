8ad811b84c2c98b34655345475c80e5e11be5c4c
Tom Schnaars <tschnaars@shift4.com>
2022-12-22 22:45:44 -0500

    init comment form

--- app/components/ui/rightbar/comment-form.js
+++ app/components/ui/rightbar/comment-form.js
@@ -1,3 +1,21 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+export default class UiRightbarCommentFormComponent extends Component {
+	constructor() {
+		super(...arguments);
+		this.comment = null;
+	}
 
-export default class UiRightbarCommentFormComponent extends Component {}
+	@service store;
+
+	@action submitComment() {
+		const comment = this.store.createRecord('comment', {
+			comment: this.comment,
+			postId: '6b62fa2a-13d1-465e-bd1d-7442b89cb47a',
+			contentId: '1d1015e4-acb0-44ba-ae9c-556bb5d34ff4',
+			post: this.args.post,
+		});
+		comment.save();
+	}
+}
