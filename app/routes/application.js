085b60f262b74566b65d143c5efe7dc398dacba9
Tom Schnaars <tschnaars@shift4.com>
2022-12-29 13:11:03 -0500

    remove clearing action from nav-login component and app route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,5 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import { action } from '@ember/object';
 
 export default class ApplicationRoute extends Route {
 	@service router;
@@ -10,8 +9,5 @@ export default class ApplicationRoute extends Route {
 		if (this.token.token && this.token.address) {
 			this.router.transitionTo('content');
 		}
-	}
-	@action clearToken() {
-		this.router.transitionTo('/');
 	}
 }
