4dd20b62f3de108f914f34b811ceb9fbaa3ecbed
Tom Schnaars <tschnaars@shift4.com>
2022-12-29 23:10:22 -0500

    tbc token service refactor

--- tests/unit/services/token-test.js
+++ tests/unit/services/token-test.js
@@ -3,10 +3,19 @@ import { setupTest } from 'llamas-app/tests/helpers';
 
 module('Unit | Service | token', function (hooks) {
 	setupTest(hooks);
-
-	// TODO: Replace this with your real tests.
 	test('it exists', function (assert) {
 		let service = this.owner.lookup('service:token');
 		assert.ok(service);
 	});
+	// TODO: Decide how the transition should work.  This token service is gross right now
+	//
+	// test('it clears token', function (assert) {
+	// 	let service = this.owner.lookup('service:token');
+	// 	localStorage.setItem('token', '1234');
+	// 	localStorage.setItem('address', '1234');
+
+	// 	service.clear();
+
+	// 	assert.ok(service);
+	// });
 });
