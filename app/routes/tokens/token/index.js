464746360d65a8649d99b767c67e190bb2147557
Tom Schnaars <tschnaars@shift4.com>
2023-01-21 22:25:42 -0500

    handle empty posts and moving around models

--- app/routes/tokens/token/index.js
+++ app/routes/tokens/token/index.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
-export default class TokensTokenPostsRoute extends Route {}
+export default class TokensTokenPostsRoute extends Route {
+	@service store;
+
+	model() {
+		const token = this.modelFor('tokens.token');
+		return hash({
+			token,
+			posts: this.store.query('post', { contentId: token.id }),
+		});
+	}
+}
