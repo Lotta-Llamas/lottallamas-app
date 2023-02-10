44ab7944b51f118f0f32707284743b640cbd9a78
Tom Schnaars <tschnaars@shift4.com>
2023-02-09 20:50:54 -0500

    post component

--- app/components/posts/post.js
+++ app/components/posts/post.js
@@ -1,11 +1,8 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
 
 export default class PostsPostComponent extends Component {
-	@tracked isEditing = false;
-
 	@service router;
 
 	@action editPost() {
