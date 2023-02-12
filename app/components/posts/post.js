7d272fdaa7869fea1568cde50e8d7b8222b5e317
Tom Schnaars <tschnaars@shift4.com>
2023-02-12 02:44:13 -0500

    do not allow non owner to edit post

--- app/components/posts/post.js
+++ app/components/posts/post.js
@@ -5,6 +5,12 @@ import { service } from '@ember/service';
 export default class PostsPostComponent extends Component {
 	@service router;
 
+	@service token;
+
+	get canEdit() {
+		return this.token.address === this.args.post.walletId;
+	}
+
 	@action editPost() {
 		this.router.transitionTo('tokens.token.posts.edit', this.args.post.id);
 	}
