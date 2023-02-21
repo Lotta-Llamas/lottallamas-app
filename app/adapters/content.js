7bfe028818c81803d7f8719771e56011fcf50d00
Tom Schnaars <tschnaars@shift4.com>
2023-02-21 12:18:28 -0500

    add br

--- app/adapters/content.js
+++ app/adapters/content.js
@@ -1,6 +1,7 @@
 import RESTAdapter from '@ember-data/adapter/rest';
 import { singularize } from 'ember-inflector';
 import ENV from 'llamas-app/config/environment';
+
 export default class ApplicationAdapter extends RESTAdapter {
 	host = ENV.host;
 	namespace = 'api';
